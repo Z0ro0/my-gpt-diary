0e368a9325f7ff7c4146ba017e9a240e5558c26e
Z0ro0 <chlrkdmf0805@gmail.com>
2023-10-12 07:29:23 +0900

    counter 예제

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [userInput, setUserInput] = useState("");
+
+  const handleClickPlus = () => {
+    setCount(count + 1);
+  };
+  const handleClickMinus = () => {
+    setCount(count - 1);
+  };
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value); // Update the userInput state with the input value
+  };
+  
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      const num = Number(userInput);
+      if (Number.isInteger(num)) {
+        setCount(num);
+        setUserInput(""); // Clear the input field
+      }
+    }
+  };
 
   return (
     <>
+      <div>current count: {count}</div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>count value input: </div>
+        <input
+          value={userInput}
+          onChange={handleUserInput}
+          onKeyDown={handleEnter} // Use handleEnter for Enter key press
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>buttons: </div>
+      <button onClick={handleClickPlus}>+</button>
+      <button onClick={handleClickMinus}>-</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
